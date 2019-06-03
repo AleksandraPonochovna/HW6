be5e60f120a6534a9634ae58f8edaa13d6960094
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-06-03 19:19:39 +0300

    Update CarApp.java

--- CarApp.java
+++ CarApp.java
@@ -1,1 +1,10 @@
+package com.company;
 
+public class CarApp {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Car audiCar = new Car("Audi", "Q7", "Black", 200, 2015);
+        Car audiCloneCar = (Car) audiCar.clone();
+        System.out.println(audiCar.toString());
+        System.out.println(audiCloneCar.toString());
+    }
+}
