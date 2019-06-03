a33bedc81c7a6061d13633fe8ff860955bcdeef0
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-06-03 16:46:25 +0300

    Update RobotApplication.java

--- RobotApplication.java
+++ RobotApplication.java
@@ -1,1 +1,26 @@
+package com.company;
+
+public class RobotApplication {
+
+    public static void main(String[] args) {
+        RobotConnectionManager robotConnection = null;
+        moveRobot(robotConnection, 4, 5);
+    }
+
+    public static void moveRobot(RobotConnectionManager robotConnectionManager, int toX, int toY) {
+        boolean check = false;
+        for (int i = 1; !check && i <= 3; i++) {
+            try (RobotConnection robotConnection = robotConnectionManager.getConnection()) {
+                robotConnection.moveRobotTo(toX, toY);
+                check = true;
+            } catch (RobotConnectionException ex) {
+                if (!check && i == 3) {
+                    throw new RobotConnectionException("Close connection.");
+                }
+            } catch (RuntimeException ex) {
+                System.out.println("Error.");
+            }
+        }
+    }
+}
 
